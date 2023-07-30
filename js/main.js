3b6aba9197c1a321d9786cffa07521b1d1898752
john safwat <john362sss@gmail.com>
2023-07-30 10:55:24 +0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   css/style.css #	new file:   images/background pattern.png #	new file:   images/banner png/chat.png #	new file:   images/banner png/gaya store.png #	new file:   images/banner png/movies 1.png #	new file:   images/banner png/movies 2.png #	new file:   images/banner png/news.png #	new file:   images/banner png/todo.png #	new file:   images/banners/chat.png #	new file:   images/banners/gaya store.png #	new file:   images/banners/movies 1.png #	new file:   images/banners/movies 2.png #	new file:   images/banners/news.png #	new file:   images/banners/todo.png #	new file:   images/decoration.png #	new file:   images/icons/behance.png #	new file:   images/icons/discord.png #	new file:   images/icons/facebook.png #	new file:   images/icons/github.png #	new file:   images/icons/google.png #	new file:   images/icons/instagram.png #	new file:   images/icons/linkedin.png #	new file:   images/icons/whatsapp.png #	modified:   index.html #	modified:   js/main.js #	modified:   webfonts/fa-brands-400.ttf #	modified:   webfonts/fa-brands-400.woff2 #	modified:   webfonts/fa-regular-400.ttf #	modified:   webfonts/fa-regular-400.woff2 #	modified:   webfonts/fa-solid-900.ttf #	modified:   webfonts/fa-solid-900.woff2 #	modified:   webfonts/fa-v4compatibility.ttf #	modified:   webfonts/fa-v4compatibility.woff2 #

---
+++ js/main.js
@@ -0,0 +1,93 @@
+
+var projects = [
+    {
+        "image" : "images/banners/gaya store.png",
+        'title' : 'Gaya Store App',
+        'description' : "A Flutter e-commerce app that allows users to browse and purchase products from a variety of categories. The app features a clean and user-friendly UI, as well as a variety of features, such as product filters, product reviews, and a shopping cart. Build to demonstrate the use of flutter and (Clean Arch , MVVM ,Repository Pattern , SQLite )",
+        'link' : 'https://github.com/john-safwat/Gaya-Store-App',
+        'downLoadLink' : 'https://github.com/john-safwat/Gaya-Store-App/releases/download/GayaStore/Gaya.Store.apk',
+
+    },
+    {
+        "image" : "images/banners/movies 2.png",
+        'title' : 'Movies App',
+        'description' : "A Flutter movies app that allows users to browse and watch movies from a variety of sources. Build to demonstrate the use of flutter and (Dio , MVVM , Bloc , Clean Arch)",
+        'link' : 'https://github.com/john-safwat/Movies',
+        'downLoadLink' : 'https://github.com/john-safwat/Movies/releases/download/Movies/movies.apk',
+    },
+    {
+        "image" : "images/banners/chat.png",
+        'title' : 'Chat App',
+        'description' : "This chat app is a Flutter project that uses Firebase for authentication, data storage, and real-time communication. It is built using the Clean Architecture pattern, which separates the app's concerns into different layers. The Repository Pattern is used to manage the app's data, and the MVVM (Model-View-ViewModel) pattern is used to manage the app's UI.",
+        'link' : 'https://github.com/john-safwat/Chat',
+        'downLoadLink' : 'https://github.com/john-safwat/Chat/releases/download/ChatApp/chat.apk',
+    },
+    {
+        "image" : "images/banners/movies 1.png",
+        'title' : 'Movies App',
+        'description' : "A Flutter movies app that allows users to browse and watch movies from a variety of sources. Build to demonstrate the use of flutter",
+        'link' : 'https://github.com/john-safwat/MoviesApp',
+        'downLoadLink' : 'https://github.com/john-safwat/MoviesApp/releases/download/MoviesApp/Movies.App.apk',
+    },
+
+    {
+        "image" : "images/banners/todo.png",
+        'title' : 'ToDo App',
+        'description' : "is a sample task management flutter application built to demonstrate the use of flutter with firebase",
+        'link' : 'https://github.com/john-safwat/ToDo',
+        'downLoadLink' : 'https://github.com/john-safwat/ToDo/releases/download/ToDoAppv2/todo.apk',
+    },
+    {
+        "image" : "images/banners/news.png",
+        'title' : 'News App',
+        'description' : "API News is JSON API For Live News and Blog Articles from the media. This Project uses the HTTP Package to connect FlutterApplication with REST API",
+        'link' : 'https://github.com/john-safwat/NewsApp',
+        'downLoadLink' : 'https://github.com/john-safwat/NewsApp/releases/download/NewsAppv2/news.app.apk',
+    }
+]
+
+
+for(let i = 0; i<projects.length ; i++){
+    document.getElementById('projects').innerHTML +=`<div class="container project"><img src="${projects[i]['image']}" alt=""><div class="content"><p class="title">${projects[i]['title']}</p><p class="description">${projects[i]['description']}</p><a href="${projects[i]['link']}" target="_blank" class="link">View Project</a><a href="${projects[i]['downLoadLink']}" target="_blank" class="downLoadLink">DownLoad Now</a></div></div>`;
+}
+
+
+var links = [
+    {
+        'link' :"https://github.com/john-safwat" ,
+        'icon' :"images/icons/github.png"
+    },
+    {
+        'link' :"https://www.linkedin.com/in/john-safwat-b3645427a/" ,
+        'icon' :"images/icons/linkedin.png"
+    },
+    {
+        'link' :"https://www.behance.net/johnsafwat" ,
+        'icon' :"images/icons/behance.png"
+    },
+    {
+        'link' :"mailto:johnsafwat362@gmail.com" ,
+        'icon' :"images/icons/google.png"
+    },
+    {
+        'link' :"https://www.instagram.com/john_s_911/" ,
+        'icon' :"images/icons/instagram.png"
+    },
+    {
+        'link' :"https://discordapp.com/users/787110639083978833 " ,
+        'icon' :"images/icons/discord.png"
+    },
+    {
+        'link' :"https://wa.me/01204984211" ,
+        'icon' :"images/icons/whatsapp.png"
+    },
+    {
+        'link' :"https://www.facebook.com/john.safwat.77" ,
+        'icon' :"images/icons/facebook.png"
+    },
+];
+
+for(let i = 0; i<links.length ; i++){
+    document.getElementById('icons').innerHTML +=`<a href="${links[i]['link']}" target="_blanc"><img src="${links[i]['icon']}" alt=""></a>`;
+
+}
